fb8877bf096bc674bdac4d100002ff2859e69a5a
Michael Thornton <msthornton@Underverse.local>
2014-08-04 16:51:11 -0500

    updated to middleclass 3.0

--- cipr/class/init.lua
+++ cipr/class/init.lua
@@ -1,14 +1,17 @@
 local cipr = require 'cipr'
 
--- Load middleclass in a sandbox
-local function middleclass()
-    require 'middleclass/middleclass'
-    return {
-        class = class,
-        subclassOf = subclassOf,
-        instanceOf = instanceOf,
-        includes = includes,
-    }
-end	
+-- -- Load middleclass in a sandbox
+-- local function middleclass()
+--     require 'middleclass.middleclass'
+--     return {
+--         class = class,
+--         subclassOf = subclassOf,
+--         instanceOf = instanceOf,
+--         includes = includes,
+--     }
+-- end	
 
-return middleclass()
+-- return middleclass()
+
+local middleclass = cipr.import 'middleclass.middleclass'
+return middleclass
